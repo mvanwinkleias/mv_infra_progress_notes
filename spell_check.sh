e889ae5dbfa4101f6643a8bfb750fdfd8a3bb1ba
Martin VanWinkle <mvanwinkle@ias.edu>
2018-11-16 12:36:51 -0500

    checking this in.

--- spell_check.sh
+++ spell_check.sh
@@ -6,9 +6,9 @@ ASPELL_PROJECT_FILE=$bin_whence/aspell_project.pws
 
 find ./ -type f \
 | egrep -i '(.md)$' \
-| xargs -n1 -i sh -c "aspell --mode=sgml -p $ASPELL_PROJECT_FILE -c \"{}\" < /dev/tty"
+| xargs -n1 -i sh -c "aspell --dont-backup --mode=sgml -p $ASPELL_PROJECT_FILE -c \"{}\" < /dev/tty"
 
 find ./ -type f \
 | egrep -vi '(.md)$' \ 
-| xargs -n1 -i sh -c "aspell -p $ASPELL_PROJECT_FILE -c \"{}\" < /dev/tty"
+| xargs -n1 -i sh -c "aspell --dont-backup -p $ASPELL_PROJECT_FILE -c \"{}\" < /dev/tty"
 
